a0ffc46dd01afe1f02f0dbbc339f69555899cea3
kurry421 <kurry@Karrans-MBP.fios-router.home>
2016-11-21 17:18:53 -0500

    init

---
+++ config.php
@@ -0,0 +1,7 @@
+
+<?php
+// see README.md for an explanation of these config params
+$TWILIO_ACCOUNT_SID = 'AC7190fdf4b67b79537274029fc0daa4ff';
+$TWILIO_CONFIGURATION_SID = 'VSa19d80296dd30b91cc7cc297e1f9972b';
+$TWILIO_API_KEY = 'SK0f03b23e0eeaaed43e7faaa4aee93754';
+$TWILIO_API_SECRET = 'eTqoePF2iCVSjYCl5t4U2qwg3N6YUlvG';
